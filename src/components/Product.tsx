a0f44a8f56eacef7c59f85d4ebaba7bc3efce8d9
Vika <viktoriav.aero@gmail.com>
2022-10-25 23:59:23 +0300

    added products

---
+++ src/components/Product.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { IProduct } from '../models';
+
+interface ProductProps {
+    product: IProduct
+}
+
+const Product = (props: ProductProps) => {
+    
+    return (
+        <div
+            className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+        >
+            {props.product.title}
+        </div>
+    );
+};
+
+export default Product;
