d97600dc2d40dcbd89642e7bdd434a88b1a2002d
Vika <viktoriav.aero@gmail.com>
2022-10-27 22:32:41 +0300

    added navigation, routes

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla dolorem alias laborum fugiat sunt veritatis amet reiciendis ex harum. Modi aut inventore eaque maxime doloremque placeat cumque sequi, quidem ratione nemo, velit aspernatur sint, a totam temporibus adipisci pariatur tenetur reprehenderit harum ad eos vel? Modi ipsa quas ratione minima?</div>
+    )
+}
